d2e3c8593728f9b3cb3277f9d573f4e4bfc53bf0
Devashish <devashish19jha@gmail.com>
2024-01-15 18:12:38 +0530

    end to end

--- src/test/java/com/example/demo/ProductSpringAngularConnectionApplicationTests.java
+++ src/test/java/com/example/demo/ProductSpringAngularConnectionApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProductSpringAngularConnectionApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
